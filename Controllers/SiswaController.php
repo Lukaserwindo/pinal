303cce5588904bbb17e1ad9dedbea160189fb1c4
Lukaserwindo <lukassnake558@gmail.com>
2020-09-18 11:19:38 +0700

    first commit

---
+++ Controllers/SiswaController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Siswa;
+use Illuminate\Support\Facades\Validator;
+
+class SiswaController extends Controller
+{
+  public function store(Request $request)
+  {
+    $validator=Validator::make($request->all(),
+    [
+      'nama_siswa'=> 'required', 'tanggal_lahir' => 'required', 'gender' => 'required', 'alamat' => 'required', 'id_kelas' => 'required'
+    ]
+  );
+
+  if($validator->fails()) {
+    return Response()->json($validator->errors());
+  }
+
+  $simpan = Siswa::create([
+    'nama_siswa' => $request->nama_siswa,
+    'tanggal_lahir' => $request->tanggal_lahir,
+    'gender' => $request->gender,
+    'alamat' => $request->alamat,
+    'id_kelas'=> $request->id_kelas
+  ]);
+
+  if($simpan)
+   {
+     return Response()->json(['status' => 1]);
+
+  }
+  else
+  {
+    return Response()->json(['status' =>0]);
+  }
+  }
+
+}
